386086e3c46b5ed38a555c38bb16244cb8cdf88a
APPENNINOCASA <appenninocasa@gmail.com>
2025-11-05 00:07:20 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Appartamenti in Appennino</h1>
+        <p>Sistema di Gestione Immobili</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
