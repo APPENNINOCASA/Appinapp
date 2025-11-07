278f8f19b3c339e68de70724b3d168da311944a2
APPENNINOCASA <appenninocasa@gmail.com>
2025-11-08 00:40:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import PropertyImages from './pages/PropertyImages'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './contexts/AuthContext'
 import Login from './pages/Login'
@@ -5,6 +6,12 @@ import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import AddProperty from './pages/AddProperty'
 import MyProperties from './pages/MyProperties'
+<Route path="/property-images/:id" element={
+  <ProtectedRoute>
+    <PropertyImages />
+  </ProtectedRoute>
+} />
+
 import './App.css'
 
 function ProtectedRoute({ children }) {
